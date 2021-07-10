712ac93084097748ca81a633f142e6c32eda4d7d
dchiapa <dchiapa@est.frba.utn.edu.ar>
2021-07-10 00:11:03 -0300

    actualizar envio de formulario

--- pages/contacto.php
+++ pages/contacto.php
@@ -9,5 +9,5 @@ $headers = 'From: ' . $email_to . "\r\n" . 'X-Mailer: PHP/' . phpversion();
 @mail($email_to, $email_subject, $email_message, $headers);
 echo '<script type="text/javascript">
 		    	alert("Gracias por su consulta, me comunicaré a la brevedad");
-		    	window.location.href="/index.html";
+		    	window.location.href="../index.html";
 		      </script>';
